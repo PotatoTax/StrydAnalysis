5b15661a25b2f49b45e115afd4febbfc10b89056
Conor <conor@johngregg.org>
2020-08-06 22:46:09 -0500

    Power graph displayed

--- app/views.py
+++ app/views.py
@@ -11,38 +11,38 @@ def index(request):
     return render(request, 'activities/index.html', context)
 
 
-def activity(request, activity_id, chart_id='chart_id', chart_height=500):
+def activity(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
 
     chart = {
-        "render_to": chart_id,
+        "render_to": 'chart_id',
         "type": 'line',
-        "height": chart_height
+        "height": 500
     }
     title = 'Activity Data'
-    xAxis = {
+    x_axis = {
         "title": {"text": "Time"},
         "categories": list(range(int(activity.timer_time)))
     }
-    yAxis = {
+    y_axis = {
         "title": {"text": 'Power'},
         "categories": list(range(0, activity.max_power))
     }
     series = [
         {
             "name": "Power",
-            "data": [r.power for r in Record.objects.filter(activity=activity)]
+            "data": [r.__getattribute__('power') for r in Record.objects.filter(activity=activity)]
         }
     ]
 
     context = {
         'activity': activity,
-        'chart_id': chart_id,
+        'chart_id': 'chart_id',
         'chart': chart,
         'series': series,
         'title': title,
-        'xAxis': xAxis,
-        'yAxis': yAxis
+        'xAxis': x_axis,
+        'yAxis': y_axis
     }
 
     return render(request, 'activities/activity.html', context)
